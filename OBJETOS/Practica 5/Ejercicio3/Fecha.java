c47d66ae0d83c12f13c52172f9a1d2933b5927ef
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-12-22 17:37:39 -0300

    Create Fecha.java

---
+++ OBJETOS/Practica 5/Ejercicio3/Fecha.java
@@ -0,0 +1,15 @@
+package ejercicio3practica5n;
+
+public class Fecha {
+    private String ciudad;
+    private String dia;
+
+    public Fecha(String ciudad, String dia) {
+        this.ciudad = ciudad;
+        this.dia = dia;
+    } 
+    
+    public String getCiudad() {
+        return ciudad;
+    }
+}
